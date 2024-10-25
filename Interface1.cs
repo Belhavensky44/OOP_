2fdd8a5304b2263e64986fdde86f967a836990d4
Belhavensky <Schuleeee123@yandex.ru>
2024-10-25 18:38:02 +0500

    Добавьте файлы проекта.

---
+++ Interface1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP_
+{
+    public interface IHighInterfaceArray
+    {
+        bool Find(long searchValue);
+        void Insert(long value);
+        bool Delete(long value);
+        void Display();
+        int GetSize();
+        void FindMinMax();
+    }
+}
